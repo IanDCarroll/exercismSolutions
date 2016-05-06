39814abaed946956962577f9bc72bcb18e4193e1
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-06 13:09:45 -0700

    refactor hamming.js

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,17 +1,15 @@
 function Hamming() {
     this.compute = function(strandA, strandB) {
-	var hamCount = 0;
 	if (strandA.length != strandB.length) {
 	    throw new Error("DNA strands must be of equal length.");
 	}
-	else {
-	    var arrayA = strandA.split(""),
-		arrayB = strandB.split("");
-	    for (var i = 0; i < arrayA.length; i++) {
-		if (arrayA[i] !== arrayB[i]) { hamCount += 1; }
-	    }
-	    return hamCount;
+	var hamCount = 0,
+	    arrayA = strandA.split(""),
+	    arrayB = strandB.split("");
+	for (var i = 0; i < arrayA.length; i++) {
+	    if (arrayA[i] !== arrayB[i]) { hamCount += 1; }
 	}
+	return hamCount;
     }
 }
 
