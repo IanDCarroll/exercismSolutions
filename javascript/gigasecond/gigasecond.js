31e268c93211f1e47ee690afb752048e332902b9
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-16 12:28:52 -0700

    work gigasecond problem

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,10 +1,14 @@
 //in progress
 
 function Gigasecond() {
+    //The test suite is passing a date object as the argument for birthdate.
+    //I was hoping that object would evaluate itself to a usable number.
+    //but that's not how JS works. Returns NaN.
     this.date = function(birthdate) {
-	var msDate = Date.parse(birthdate),
-	    msInGS = 100000000000,
-	    aniversary = Date.toString(msDate + msInGS);
+	//write something that can take the date object 
+	//and get it's ms value.
+	var msInGS = 100000000000,
+	    aniversary = birthdate + msInGS;
 	return aniversary;
    }
 };
