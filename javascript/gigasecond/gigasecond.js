80207a6a0b43629ec54f586406b53787ba22f518
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-20 00:26:08 -0700

    add polish and explaination to js gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,16 +1,31 @@
-//in progress
 
-function Gigasecond(date) {
+
+function Gigasecond(startDate) {
 
     this.date = function() { 
-	var niceName = Date.parse(date),
-	    msInGS = 1000000000000,
-	    aniversary = niceName + msInGS,
-	    outDamnSpot = new Date(aniversary);
-	return outDamnSpot;
+	var startDateMS = Date.parse(startDate),
+	    oneGSinMS = 1000000000000,
+	    gsAnivMS = startDateMS + oneGSinMS,
+	    gsAniv = new Date(gsAnivMS);
+	return gsAniv;
     }
    
 };
 
-//console.log(Gigasecond(new Date(Date.UTC(2015, 8, 14))));
 module.exports = Gigasecond;
+
+
+/* The JS Date function reckons time in miliseconds
+ * from 1/1/1970 00:00:000.
+ * 
+ * But when a date function is passed as an arg 
+ * it passes a string.
+ * 
+ * Date.parse() will take a string and convert it to ms.
+ *
+ * We then add the requisite billion seconds 
+ * (as a trillion ms) to that number.
+ *
+ * new Date() will take our new ms number and ouala! 
+ * A new date string with the gigasecond aniversary.
+ */
