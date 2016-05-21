b52b1d2530f0837aaa3f6e0267679563a10454e6
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-21 00:59:21 -0700

    prettify var declarations

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,22 +1,22 @@
 function Words() {};
 
-Words.prototype.count = function(string) {
+Words.prototype.count = function(sentence) {
 
-    var phrase = string.split(/\s/);
-	console.log(phrase);
+    var wordList = sentence.split(/\s/);
+	console.log(wordList);
 
-    var phrases = {};
+    var results = {};
 
-    for (i=0; i> phrase.length; i++) { 
-	for (var key in Object.keys(phrase)) {
-		console.log(key);
-		if (key === phrase[i]) {
-			phrases.key += 1;
+    for (i=0; i> wordList.length; i++) { 
+	for (var word in Object.keys(wordList)) { //<--?
+		console.log(word);
+		if (word === wordList[i]) {
+			results.word += 1;
 		} 
-		else { phrases.phrase[i] = 1; }	
+		else { results.wordList[i] = 1; }	
 	}
     }
-    return phrases;
+    return results;
 };
 
 //console.log(Words.count("This is a thing."));
