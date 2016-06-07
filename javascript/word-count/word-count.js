0884f6996bf5b7ad08608f4d5fb26027934babb0
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-07 14:13:59 -0700

    JS word-count passes 11 of 12 tests

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,25 +1,47 @@
 //still working on this
+//passes all but the last test now.
 
 function Words() {};
 
 Words.prototype.count = function(sentence) {
+    sentence = sentence.toLowerCase();
+    sentence = sentence.replace(/^\s+/,'');
+    sentence = sentence.replace(/\s+$/,'');
 
-    var wordList = sentence.split(/\s/);
-	//working
-
-    var results = {};
+    var wordList = sentence.split(/\s+/),
+	results = {};
 
     for (i = 0; i < wordList.length; i++) { 
-	for (var word in Object.keys(results)) { //<--?
-		console.log(word);
-		if (word === wordList[i]) {
-			results[word] += 1;
-		} 
-		else { results[wordList[i]] = 1; }	
+	if (results[(wordList[i])]) {
+	    results[(wordList[i])] += 1;
+	} else { 
+	    results[(wordList[i])] = 1; 
 	}
     }
     return results;
 };
 
-//console.log(Words.count("This is a thing."));
 module.exports = Words;
+
+/* To pass a key into an object in JS:
+ * (an equivalent to a pythonian 'dictionary')
+ * 
+ * var thing = {}; //Declare the variable
+ * var putItIn = 'theGreatAnswer'; //Define the key
+ * thing[(putItIn)] = 42;
+ * console.log(thing); //==> { theGreatAnswer : 42 }
+ *
+ * Super Important! Use bracket notation [] 
+ *		    NO dots!	thing.thisIsntGoingToWork;
+ *		    		thing.(neitherIsThis);
+ *				//==> throws error.
+ *				thing.unlessThis_IS_theKey;
+ *				//==> { unlessThis_IS_theKey : Undefined }
+ *		    with no quotes. 
+ *		    NOT: 	thing['oldKeyAccess'];
+ *
+ *		    but WITH parentheses in the brackets ()
+ *
+ *	thing[(newKey)] = newValue; 
+ *	//==> { newKey : newValue }
+ */
