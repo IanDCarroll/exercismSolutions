1e0f73d0302883646a7270e37aa971721edfd587
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-17 06:49:25 -0700

    work on getting gradle up and running

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,2 +1,14 @@
 public class HelloWorld {
+    public static String hello(String name) {
+	return "";
+    }
 }
+/*
+Having trouble getting gradle up and running.
+Java8 compatibility error? Should be fixed by now.
+Latest error:
+
+* What went wrong:
+Execution failed for task ':test'.
+> failed to read class file //PATH WENT HERE
+*/
