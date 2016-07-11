680fbd11428e610d71594616b540360e9ed1044e
IanDCarroll <ian.d.carroll@gmail.com>
2016-07-11 11:48:14 -0700

    add isogram.js under construction

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,12 @@
+//under construction, but up and running
+
+function isogram(word) {
+    this.isIsogram = function() { 
+	if(word) {
+	    return true;
+	} else {
+	    return false;
+	} 
+    }
+}
+module.exports = isogram;
