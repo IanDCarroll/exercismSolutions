067548093567d442218dc6bd8690223b327ef6f4
IanDCarroll <ian.d.carroll@gmail.com>
2016-08-23 10:04:46 -0700

    clean up rust hello-world comments

--- rust/hello-world/src/lib.rs
+++ rust/hello-world/src/lib.rs
@@ -3,18 +3,20 @@ pub fn hello(name: Option<&str>) -> String {
     format!("Hello, {}!", n)
 }
 
-/* This is a line-condensed version of the script I paired with @kammitama5 
-   at the Rust LA meetup. It was paired between the two of us with major 
-   help and credit going to @hjr3 and many other more seasoned rustaceans. 
-   Thanks for the great meetupi!
+/* This is a line-condensed version of the script I paired with 
+   Krystal @kammitama5 at the Rust LA meetup with major help and 
+   credit going to Herman Radtke @hjr3 and Matt Kraai. 
+   Thanks for the great meetup!
 
-   After playing around a little bit, I can say that unlike python, 
-   whitespace is less of a deal for propper syntax. Like C or JS. 
+   After playing around a little bit, I can say that unlike 
+   Python, whitespace is less of a deal for propper syntax, 
+   like in C or JS. 
    As I learn more I'll reevaluate that hypothesis.
 
-   Todo: Go through each element of this syntax and comment on what it is 
-   and what it does. 
+   Todo: Go through each element of this syntax and comment on 
+   what it is and what it does. 
    Comment on the nature of rust as a whole, 
    and how this function traverses its waters.
    Start reading the rust book and doing the exercises therein.
  */
+
