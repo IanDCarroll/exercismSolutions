67f47f23739a8a119b25c279be74b7d2e522c0f2
Ian Carroll <ian.d.carroll@gmail.com>
2018-06-20 22:23:16 -0700

    Refactor gigasecond.js

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,32 +1,27 @@
+class Gigasecond {
+  constructor(inputDate) {
+    this.inputMiliseconds = Date.parse(inputDate);
+  }
 
+  date() {
+    return new Date(this.gigasecondize(this.inputMiliseconds));
+  }
 
-function Gigasecond(startDate) {
-
-    this.date = function() { 
-	var startDateMS = Date.parse(startDate);
-	console.log(startDateMS);
-	var oneGSinMS = 1000000000000,
-	    gsAnivMS = startDateMS + oneGSinMS,
-	    gsAniv = new Date(gsAnivMS);
-	return gsAniv;
-    }
-   
-};
+  gigasecondize(unixMiliseconds) {
+    const oneTrillionMiliseconds = 1000000000000;
+    return unixMiliseconds + oneTrillionMiliseconds;
+  }
+}
 
 module.exports = Gigasecond;
 
 
-/* The JS Date function reckons time in miliseconds
- * from 1/1/1970 00:00:000.
- * 
- * But when a date function is passed as an arg 
- * it passes a string.
- * 
- * Date.parse() will take a string and convert it to ms.
- *
- * We then add the requisite billion seconds 
- * (as a trillion ms) to that number.
- *
- * new Date() will take our new ms number and ouala! 
- * A new date string with the gigasecond aniversary.
- */
+/*
+TDD
+
+1. Write tests
+2. Fail test
+3. Write code
+4. Test 1
+
+*/
